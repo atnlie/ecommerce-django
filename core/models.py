0def6d15281767c85aa568d069495743702af514
Anton <heni.kurniyanto@kuncie.com>
2023-04-05 18:36:41 +0700

    feat(orderModel): create order models item, orderItem and order

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    
+    def __self__(self):
+        return self.title
+
+class OrderItem(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+
+    def __self__(self):
+        return self.title
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DatetimeField(auto_now_add=True)
+    ordered_date = models.DatetimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __self__(self):
+        return self.user.username
+
