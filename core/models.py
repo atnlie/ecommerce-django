0d29ccd565f1f9e632913053c5836bd3a2f4a28b
Anton <heni.kurniyanto@kuncie.com>
2023-04-05 19:18:37 +0700

    feat(model): add categories of label dan category

--- core/models.py
+++ core/models.py
@@ -1,9 +1,24 @@
 from django.conf import settings
 from django.db import models
 
+CATETORY_CHOICES = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport wear'),
+    ('OW', 'Outwear')
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
+
+
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    category = models.CharField(choices=CATETORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
 
     def __self__(self):
         return self.title
