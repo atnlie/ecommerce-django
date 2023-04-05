28dff842ce233a51441941c9956e3f0f54329b58
Anton <heni.kurniyanto@kuncie.com>
2023-04-05 08:31:19 +0700

    feat(index): add index templates and regesiter ecomm

---
+++ core/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.views import generic
+
+class HomeView(generic.TemplateView):
+    template_name = "index.html"
--- ecomm/urls.py
+++ ecomm/urls.py
@@ -17,9 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name='home'),
 ]
 
 if settings.DEBUG:
