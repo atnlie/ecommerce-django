9eb04dfab7b5e7da5a7c51c6b46f9a34c89b11b1
Anton <heni.kurniyanto@kuncie.com>
2023-04-05 17:06:05 +0700

    feat(template): add product and checkout page

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomeView.as_view(), name='home'),
+    # path('', views.HomeView.as_view(), name='home'),
+    path('', include('core.urls', namespace='core')),
 ]
 
 if settings.DEBUG:
