fca793f7903b0a7c26c98789ecb0c57eacdd01e2
Anton <heni.kurniyanto@kuncie.com>
2023-04-05 07:15:53 +0700

    feat(staticConfig): setup static file and env for database config

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=*gk@*=u@$u4c+zju79%x%138#9&)e!e6gc5ysfccobu&5$z4*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,9 +115,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG is False:
+    ALLOWED_HOSTS = ['www.domain-kita.com']
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'db_name',
+            'USER': 'db_user',
+            'PASSWORD': 'db_password',
+            'HOST': 'www.domain-kita.com',
+            'PORT': '5432',
+        }
+    }
