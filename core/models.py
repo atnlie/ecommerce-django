7c13ee663375364037fd6b6ac1d72084c746976a
Anton <heni.kurniyanto@kuncie.com>
2023-04-05 19:10:03 +0700

    feat(model): create model item, oder and orderitem

--- core/models.py
+++ core/models.py
@@ -4,25 +4,33 @@ from django.db import models
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
-    
+
     def __self__(self):
         return self.title
+    
+    class Meta:
+        verbose_name_plural = 'Item'
 
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
 
-
     def __self__(self):
         return self.title
+    
+    class Meta:
+        verbose_name_plural = 'OrderItem'
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     items = models.ManyToManyField(OrderItem)
-    start_date = models.DatetimeField(auto_now_add=True)
-    ordered_date = models.DatetimeField()
+    start_date = models.DateField(auto_now_add=True)
+    ordered_date = models.DateField()
     ordered = models.BooleanField(default=False)
 
     def __self__(self):
         return self.user.username
+    
+    class Meta:
+        verbose_name_plural = 'Order'
 
--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 from django.views import generic
+# from .models import Item
 
 class HomeView(generic.TemplateView):
     template_name = "index.html"
@@ -12,3 +13,10 @@ class CheckoutView(generic.TemplateView):
 def product(request):
     template = loader.get_template('product.html')
     return HttpResponse(template.render())
+
+# def item_list(request):
+#     conteext = {
+#         'items': Item.objects.all()
+#     }
+#     template = loader.get_template('item_list.html')
+#     return HttpResponse(template.render())
